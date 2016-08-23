b90e64570baef4b583a2f279c79d42c0b4c2a4ea
rayesquire <fantastic.msp@gmail.com>
2016-08-23 14:45:25 +0800

    update

---
+++ weChat/MSPPaopaoView.m
@@ -0,0 +1,21 @@
+//
+//  MSPPaopaoView.m
+//  weChat
+//
+//  Created by 马了个马里奥 on 16/8/15.
+//  Copyright © 2016年 尾巴超大号. All rights reserved.
+//
+
+#import "MSPPaopaoView.h"
+
+@implementation MSPPaopaoView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ weChat/MSPVoiceMessageProgressView.m
@@ -0,0 +1,21 @@
+//
+//  MSPVoiceMessageProgressView.m
+//  weChat
+//
+//  Created by 马了个马里奥 on 16/8/14.
+//  Copyright © 2016年 尾巴超大号. All rights reserved.
+//
+
+#import "MSPVoiceMessageProgressView.h"
+
+@implementation MSPVoiceMessageProgressView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
